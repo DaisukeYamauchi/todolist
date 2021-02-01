4dc260bf58b22b763a58550a8e4af112c1eb4eff
DaisukeYamauchi <maveri_90@icloud.com>
2021-02-02 03:45:18 +0900

    related database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 const app = express();
 
 
@@ -12,12 +13,61 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useNewUrlParser: true});
+
+const listSchema = {
+  list_name : String
+};
+
+const Item = mongoose.model("Item", listSchema);
+//
+const item1 = new Item({
+  list_name: "welcome to your todolist!"
+});
+
+const item2 = new Item({
+  list_name: "Hit the + button to aff a new item."
+})
+
+const item3 = new Item({
+  list_name: "This is new list"
+});
+
+const defalutItems = [item1, item2, item3];
+
+// Item.insertMany(defalutItems, function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     console.log("Successfully accessed the database !");
+//   }
+// })
+
+
 
 app.get("/", function(req,res){
 
   let day = date();
 
-  res.render("list", {ListTitle: day, newListItems: items});
+  Item.find({},function(err,itemman){
+    // if(itemman.length===0){
+    //   Item.insertMany(defalutItems, function(err){
+    //     if(err){
+    //       console.log(err);
+    //     }else{
+    //       console.log("Successfully accessed the database !");
+    //     }
+    //   });
+    //   res.redirect("/");
+    // }
+    if(err){
+      console.log(err);
+    }else{
+      res.render("list", {ListTitle: day, newListItems: itemman});
+      console.log("データベースの要素の取得に成功しました。");
+    }
+  });
+
   // res.sendFile(__dirname+"/index.html");
 
 
@@ -34,6 +84,12 @@ app.get("/about", function(req,res){
 });
 
 
+app.get("/:customListName", function(req,res){
+  console.log(req.params.customListName);
+})
+
+
+
 
 
 
@@ -42,15 +98,16 @@ app.get("/about", function(req,res){
 
 
 app.post("/", function(req, res){
-  let item = req.body.wanna_list;
-  console.log(req.body.list);
-  if(req.body.list === "Work"){
-    workItems.push(item);
-    res.redirect("/work");
-  }else{
-    items.push(item);
-      res.redirect("/");
-  }
+  const itemName = req.body.wanna_list;
+  // console.log(req.body.list);
+  const item = new Item({
+    list_name: itemName
+  })
+item.save();
+
+res.redirect("/");
+
+
 
 
 
@@ -58,9 +115,21 @@ app.post("/", function(req, res){
 
   // res.render("list", {addList: item});
 
+});
+app.post("/delete", function(req,res){
+  const checkedItemId = req.body.checkbox;
+  Item.deleteOne({_id: checkedItemId },function(err){
+  if (err){
+    console.log(err);
+  }else{
+    console.log("DELETE Succsess");
+    res.redirect("/");
+  }
+
+
 })
 
-
+});
 app.listen(3001,function(){
   console.log("sever hey!");
 
